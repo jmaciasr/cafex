67adf4eb7b26471f275d09dc7c2ff5a36e96faeb
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-10-25 00:18:12 -0600

    Lazyload func

--- dev/js/gallery-lazyload.js
+++ dev/js/gallery-lazyload.js
@@ -1,1 +1,12 @@
 $("img.lazy").lazyload();
+var wScroll = $(this).scrollTop();
+
+// Landing Elements
+if (wScroll > $('.gallery').offset().top - ($(window).height() / 1.2)) {
+    $('.gallery figure').each(function (i) {
+        setTimeout(function () {
+            $('.gallery figure').eq(i).addClass('is-showing');
+        }, (700 * (Math.exp(i * 0.14))) - 700);
+    });
+
+}
